345012003ab25cfb0ab8f064bd81a1346ab5bf8e
Md Shamim Ahamed <md.shamim.ahamed.888@gmail.com>
2025-08-01 00:07:58 +0600

    update firebase Config

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -15,12 +15,12 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDAX7hIdkjNcEE6LNK0rCiAtkPeFYA7ZgE",
-  authDomain: "brainbox-ide-type.firebaseapp.com",
-  projectId: "brainbox-ide-type",
-  storageBucket: "brainbox-ide-type.firebasestorage.app",
-  messagingSenderId: "946074743090",
-  appId: "1:946074743090:web:b87d9b6ef42bb5f412672b",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 //   measurementId: "G-G3R0DLXH34"
 };
 // Initialize Firebase App.
